b64fc0ea5972d570b29e039a0726bc3574b61e96
WageehGadd <202227197@std.sci.cu.edu.eg>
2025-12-12 21:25:49 +0200

    Initial skeleton: Ready for team work

---
+++ main.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import layers, models, callbacks
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+if __name__ == "__main__":
+    print("Project started...")

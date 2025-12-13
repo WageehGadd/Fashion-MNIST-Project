9deef88940e324cab5584e8d193d2eb262c05cfb
Ahmed Hassan <106224867+Capa11@users.noreply.github.com>
2025-12-13 05:18:27 +0200

    adding dataset, required imports and preprocessing

--- main.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras import layers, models, callbacks
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-if __name__ == "__main__":
-    print("Project started...")
